3fabb5a435e7c909cc0ba5b0c1d16b08720bf37b
huochai <76243749@qq.com>
2018-11-16 16:43:57 +0800

    增加了菜单

--- app/src/main/java/com/myandroid/materialtest/MainActivity.java
+++ app/src/main/java/com/myandroid/materialtest/MainActivity.java
@@ -2,12 +2,42 @@ package com.myandroid.materialtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"点击了Backup",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.seting:
+                Toast.makeText(this,"点击了seting",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.del:
+                Toast.makeText(this,"点击了del",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
